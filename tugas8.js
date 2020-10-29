745752d4488961c13f79fbf6e5f27565b41c90c3
Muhammad Fadlil <fadlilmuhammad27@gmail.com>
2020-10-29 18:47:33 +0700

    Tugas 8 JavaScript
    Tugas ini untuk niomic bootcamp

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+var kipasAngin = {
+    merk: "sekai",
+    type: "sedang",
+    level: "5 Level",
+    harga: 250000
+};
+
+console.log(kipasAngin);
+
+kipasAngin.harga = 500000;
+
+console.log(kipasAngin);
+
